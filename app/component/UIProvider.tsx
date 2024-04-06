a6021c432263ff93372d2593e0242dfd4f5a2184
keiliving <keiliving6@gmail.com>
2024-04-06 22:18:22 +0900

    a

--- app/component/UIProvider.tsx
+++ app/component/UIProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 type UIProviderProps = {
   children: React.ReactNode;
