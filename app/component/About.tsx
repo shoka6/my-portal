a8a4f44567b8bab25daa9ece52aad45b131a377f
keiliving <keiliving6@gmail.com>
2024-04-29 21:11:47 +0900

    デザイン修正

--- app/component/About.tsx
+++ app/component/About.tsx
@@ -4,15 +4,47 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <main>
+    <main className="h-screen">
       <div className="mx-6 flex w-fit flex-col gap-2">
-        <div>name: kei</div>
+        <div>kei</div>
         <div>エンジニア 5 年目</div>
-        <Link href="https://github.com/shoka6" target="_blank">
+        <Link
+          href="https://github.com/shoka6"
+          target="_blank"
+          className="flex  text-slate-500 hover:text-slate-900"
+        >
           GitHub
+          <svg
+            aria-hidden="true"
+            className="navbar_externalArrow___VWBd"
+            height="7"
+            viewBox="0 0 6 6"
+            width="7"
+          >
+            <path
+              d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
+              fill="var(--accents-3)"
+            ></path>
+          </svg>
         </Link>
-        <Link href="https://qiita.com/keiliving" target="_blank">
+        <Link
+          href="https://qiita.com/keiliving"
+          target="_blank"
+          className="flex  text-slate-500 hover:text-slate-900"
+        >
           Qiita
+          <svg
+            aria-hidden="true"
+            className="navbar_externalArrow___VWBd"
+            height="7"
+            viewBox="0 0 6 6"
+            width="7"
+          >
+            <path
+              d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
+              fill="var(--accents-3)"
+            ></path>
+          </svg>
         </Link>
       </div>
     </main>
--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,8 +1,9 @@
-"use client";
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className="p-3"></footer>;
+  return (
+    <footer className="mt-20 justify-between border-t border-slate-200 pb-20 text-sm leading-6 text-slate-500"></footer>
+  );
 };
 
 export default Footer;
